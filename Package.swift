72eaa25f527a3e51b25d4bdeb42b47edd6064cf5
Kenneth Laskoski <kennethlaskoski@gmail.com>
2023-07-01 18:10:50 -0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "kl",
+    dependencies: [
+        .package(url: "https://github.com/kennethlaskoski/KL.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "kl",
+            dependencies: [
+                .product(name: "KL", package: "KL"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            path: "Sources"),
+    ]
+)
